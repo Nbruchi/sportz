363bdb91b9f25c441e5764f2274a4cf692fe70e5
Nbruchi <nkundabagenzibruce@gmail.com>
2026-02-07 15:36:33 +0200

    Implement matches GET and POST routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
 
 const app = express();
 const PORT = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Sportz server is running.' });
 });
+
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   const url = `http://localhost:${PORT}`;
